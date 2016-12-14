085a1c20a9b815fc7be799ef1590292627e7a047
Rinu Pullelikandathi <Rinu Pullelikandathi@MCKINSEY.COM>
2016-12-14 15:20:24 +0530

    initial commit

---
+++ src/components/TodoList.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import PureRenderMixin from 'react-addons-pure-render-mixin';
+import TodoItem from './TodoItem';
+
+export default class TodoList extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.shouldComponentUpdate = PureRenderMixin.shouldComponentUpdate.bind(this);
+  }
+  isCompleted(item) {
+   return item.get('status') === 'completed';
+  }
+  getItems(){
+    console.log(this.props.filter );
+    return this.props.todos.filter(
+      (item) => this.props.filter === "all" || item.get('status') === this.props.filter
+    );
+  }
+  render() {
+    return <section className="main">
+      <ul className="todo-list">
+        {this.getItems().map(item =>
+          <TodoItem key={item.get('text')} text={item.get('text')} isCompleted={this.isCompleted(item)}  isEditing={item.get('editing')} />
+        )}
+      </ul>
+    </section>
+  }
+};
